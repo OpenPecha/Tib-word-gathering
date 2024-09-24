cbeab3f8daa714b686ff063c8c93fd230862e324
jim-gyas <jackandthebean1234@gmail.com>
2024-09-24 12:22:02 +0530

    test format improve

--- tests/test_segpos_kangyur_and_tengyur.py
+++ tests/test_segpos_kangyur_and_tengyur.py
@@ -35,9 +35,3 @@ def test_segpos_kang_teng():
     assert (
         valid_data_json_format == expected_data
     ), f"Mismatch between processed data and expected data.\nProcessed: {valid_data_json_format}\nExpected: {expected_data}"  # noqa
-
-    print("Test passed: The source and target fields match the expected output.")
-
-
-if __name__ == "__main__":
-    test_segpos_kang_teng()
